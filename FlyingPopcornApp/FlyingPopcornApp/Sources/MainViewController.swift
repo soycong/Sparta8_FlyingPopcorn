c8789e1b6212e82e1ea6f9543957c60a7728385b
DoyleHWorks <DoyleHWorks@gmail.com>
2024-12-13 16:19:32 +0900

    [Init] 프로젝트 파일 생성
    - Main.storyboard 삭제 ( 프로젝트 설정 반영)
    - CoreData 생성 (FliyingPopcornApp.xcdatamodeld)
    - 폴더 생성 및 파일 분리: App, Resources, Sources
    - 프로젝트 설정:
      - Destination: iPhone / Min Deployment: 16.6 / Orientation: Portrait Only

---
+++ FlyingPopcornApp/FlyingPopcornApp/Sources/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  FlyingPopcornApp
+//
+//  Created by t0000-m0112 on 2024-12-13.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
