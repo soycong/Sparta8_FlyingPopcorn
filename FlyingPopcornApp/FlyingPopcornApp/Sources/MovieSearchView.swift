64fb435ff77ef4bf573ef01afaff6b940fc5e99a
soy <soysauce1223@naver.com>
2024-12-16 14:33:55 +0900

    [Add] MovieSearchView, MovieSearchViewController 파일 추가

---
+++ FlyingPopcornApp/FlyingPopcornApp/Sources/MovieSearchView.swift
@@ -0,0 +1,11 @@
+//
+//  MovieSearchView.swift
+//  FlyingPopcornApp
+//
+//  Created by seohuibaek on 12/16/24.
+//
+import UIKit
+
+final class MovieSearchView: UIView {
+
+}
---
+++ FlyingPopcornApp/FlyingPopcornApp/Sources/MovieSearchViewController.swift
@@ -0,0 +1,11 @@
+//
+//  MovieSearchViewController.swift
+//  FlyingPopcornApp
+//
+//  Created by seohuibaek on 12/16/24.
+//
+import UIKit
+
+final class MovieSearchViewController: UIViewController, UISearchResultsUpdating {
+
+}
