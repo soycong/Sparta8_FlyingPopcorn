15043d8860fc1d8870ae7c1199a5a068d5792c09
soy <soysauce1223@naver.com>
2024-12-16 16:27:16 +0900

    [Feat] movieTitleLabel, movieGenreLabel 생성 및 오토레이아웃 적용

--- FlyingPopcornApp/FlyingPopcornApp/Sources/MovieSearchCollectionViewCell.swift
+++ FlyingPopcornApp/FlyingPopcornApp/Sources/MovieSearchCollectionViewCell.swift
@@ -16,6 +16,16 @@ final class MovieSearchCollectionViewCell: UICollectionViewCell {
         //$0.layer.borderColor = UIColor.white.cgColor
     }
     
+    private let movieTitleLabel = UILabel().then {
+        $0.text = "영화 제목입니다!!"
+        $0.font = .systemFont(ofSize: 15)
+    }
+    
+    private let movieGenreLabel = UILabel().then {
+        $0.text = "영화 장르입니다!!"
+        $0.font = .systemFont(ofSize: 12)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .white
@@ -31,17 +41,31 @@ final class MovieSearchCollectionViewCell: UICollectionViewCell {
     
     private func configureUI() {
         self.addSubview(movieImageView)
+        self.addSubview(movieTitleLabel)
+        self.addSubview(movieGenreLabel)
     }
     
     private func configureConstraints() {
         movieImageView.snp.makeConstraints { make in
-            make.top.equalTo(self).offset(5) // 상단에 5의 여백 추가
-            make.leading.equalTo(self).offset(5) // 왼쪽에 5의 여백 추가
-            make.trailing.equalTo(self).offset(-5) // 오른쪽에 5의 여백 추가
-            //make.bottom.equalTo(self).offset(-5) // 하단에 5의 여백 추가
+            make.top.equalTo(self).offset(5)
+            make.leading.equalTo(self).offset(5)
+            make.trailing.equalTo(self).offset(-5)
+            make.bottom.equalTo(self).offset(-5) // 하단에 5의 여백 추가
 //            make.top.leading.trailing.equalTo(self)
 //            make.width.equalTo(itemWidth)
 //            make.height.equalTo(self.snp.width).multipliedBy(1.5) // 세로 길이를 가로 길이에 1.5배 비율로 설정
+        }
+        
+        movieTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(movieImageView.snp.bottom).offset(5)
+            make.leading.equalTo(self).offset(5)
+            make.trailing.equalTo(self).offset(-5)
+        }
+        
+        movieTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(movieTitleLabel.snp.bottom).offset(5)
+            make.leading.equalTo(self).offset(5)
+            make.trailing.equalTo(self).offset(-5)
         }
     }
 }
