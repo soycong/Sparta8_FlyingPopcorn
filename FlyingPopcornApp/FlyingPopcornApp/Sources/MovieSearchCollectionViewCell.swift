b4c9934a353506558364c179c9cd7975bdfc68ad
soy <soysauce1223@naver.com>
2024-12-16 15:20:18 +0900

    [Feat] CollectionViewCell 초기 설정

--- FlyingPopcornApp/FlyingPopcornApp/Sources/MovieSearchCollectionViewCell.swift
+++ FlyingPopcornApp/FlyingPopcornApp/Sources/MovieSearchCollectionViewCell.swift
@@ -7,5 +7,12 @@
 import UIKit
 
 final class MovieSearchCollectionViewCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .white
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
