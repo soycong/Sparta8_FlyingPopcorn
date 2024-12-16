e1246891ed7451eb4cded7f61b30db3d16a534b3
soy <soysauce1223@naver.com>
2024-12-16 20:38:57 +0900

    [Feat] SearchController 구현

--- FlyingPopcornApp/FlyingPopcornApp/Sources/MovieSearchViewController.swift
+++ FlyingPopcornApp/FlyingPopcornApp/Sources/MovieSearchViewController.swift
@@ -6,25 +6,32 @@
 //
 import UIKit
 
-final class MovieSearchViewController: UIViewController, UISearchResultsUpdating {
-    private let searchController = UISearchController(searchResultsController: nil)
+final class MovieSearchViewController: UIViewController {
     private let movieSearchView = MovieSearchView()
+    private let searchController = UISearchController()
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+
         view = movieSearchView
         
         configureUI()
     }
     
     private func configureUI() {
+        title = "Movie Search" // 네비게이션 타이틀 추가
+        view.backgroundColor = .white
+        
+        // Search Controller 설정
         searchController.searchResultsUpdater = self
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.placeholder = "Search Movies"
         navigationItem.searchController = searchController
         definesPresentationContext = true
     }
-    
+}
+
+extension MovieSearchViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        
     }
 }
