8400dba5df6ff3ff5e9df00faf11bd425e1ddf45
soy <soysauce1223@naver.com>
2024-12-17 13:16:23 +0900

    [Add] MyPageView 파일 생성

---
+++ FlyingPopcornApp/FlyingPopcornApp/Sources/MyPageView.swift
@@ -0,0 +1,13 @@
+//
+//  MyPageView.swift
+//  FlyingPopcornApp
+//
+//  Created by seohuibaek on 12/17/24.
+//
+import UIKit
+import SnapKit
+import Then
+
+final class MyPageView: UIView {
+    
+}
