05994f651fcc68c51dbdfeb8dc8ba478beee1e79
soy <soysauce1223@naver.com>
2024-12-17 09:07:11 +0900

    [Update] 영화 더미 데이터 생성

---
+++ FlyingPopcornApp/FlyingPopcornApp/Sources/DummyMovieData.swift
@@ -0,0 +1,12 @@
+//
+//  DummyMovieData.swift
+//  FlyingPopcornApp
+//
+//  Created by seohuibaek on 12/17/24.
+//
+
+struct DummyMovieData {
+    let title: String
+    let genre: String
+    let posterImageName: String
+}
