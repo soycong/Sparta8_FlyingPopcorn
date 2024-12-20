bbf1cd2e87f884f399e59b6e0afb002ca15d1944
soy <soysauce1223@naver.com>
2024-12-20 09:24:21 +0900

    [Feat] 마이페이지 로그인이 필요합니다 버튼 클릭 시, Alert 창 추가

--- FlyingPopcornApp/FlyingPopcornApp/Sources/Controllers/MyPageViewController.swift
+++ FlyingPopcornApp/FlyingPopcornApp/Sources/Controllers/MyPageViewController.swift
@@ -70,9 +70,27 @@ extension MyPageViewController {
 
 extension MyPageViewController: MyPageViewLoginDelegate {
     func myPageViewDidRequestLogin() {
-        let signinVC = SigninViewController()
-        let navController = UINavigationController(rootViewController: signinVC)
-        navController.modalPresentationStyle = .fullScreen
-        present(navController, animated: true)
+        let alert = UIAlertController(title: "로그인이 필요합니다.",
+                                      message: "로그인 하시겠습니까?",
+                                      preferredStyle: .alert)
+                
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { _ in
+            print("확인 버튼 누름")
+            
+            // 로그인 화면으로 이동
+            let signInVC = SigninViewController()
+            let signInNavController = UINavigationController(rootViewController: signInVC)
+            signInNavController.modalPresentationStyle = .fullScreen
+            
+            // 현재 윈도우 가져와서 루트 변경
+            if let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? UIWindowSceneDelegate,
+               let window = sceneDelegate.window {
+                window?.rootViewController = signInNavController
+                window?.makeKeyAndVisible()
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "취소", style: .destructive, handler: nil))
+            
+        self.present(alert, animated: true, completion: nil)
     }
 }
